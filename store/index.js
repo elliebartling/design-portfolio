ebd65d984584f6b54179ec913352b917b9a79f61
Ellie Bartling <ellenbartling@gmail.com>
2019-07-23 11:22:48 -0500

    DEBUG: axios in nuxtserverinit

--- store/index.js
+++ store/index.js
@@ -19,17 +19,18 @@ export const getters = {
 
 export const actions = {
   async nuxtServerInit({ dispatch, state }, { app }) {
-    const { data } = await app.$axios
+    const data = await app.$axios
         .get('/.netlify/functions/notion')
+        .then(res => { console.log(res) })
         .catch(err => { console.log(err) })
 
-    const { collections } = data
-
-    state.pages = find(collections, { title: 'Pages' }).items
-    state.caseStudies = find(collections, { title: 'Case Studies' })
-    state.caseStudyPosts = state.caseStudies.items
-    state.thoughts = find(collections, { title: 'Thoughts' })
-    state.thoughtsPosts = state.thoughts.items
+    // const { collections } = data
+    //
+    // state.pages = find(collections, { title: 'Pages' }).items
+    // state.caseStudies = find(collections, { title: 'Case Studies' })
+    // state.caseStudyPosts = state.caseStudies.items
+    // state.thoughts = find(collections, { title: 'Thoughts' })
+    // state.thoughtsPosts = state.thoughts.items
 
     return data
   }
